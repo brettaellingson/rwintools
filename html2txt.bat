b463ec1477592574790095824775cfb42305372a
l <l@eb6930p>
2012-07-11 16:38:57 -0500

    added mpb

--- html2txt.bat
+++ html2txt.bat
@@ -1,6 +1,7 @@
 	@echo off
 	setlocal
+	
+	if not defined WINRUBY19EXE set WINRUBY19EXE=C:\Ruby192\bin\ruby.exe
 
-	set RUBY=C:\Ruby192\bin\ruby.exe
-
-	"%RUBY%" "%~dp0html2txt.rb" %*
+:loop
+	"%WINRUBY19EXE%" "%~dp0html2txt.rb" %*
